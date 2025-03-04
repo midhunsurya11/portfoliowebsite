930ae52e3b5020d07b307cd47e2cbfd76ad99926
midhun <midhunsurya11@gmail.com>
2025-03-04 12:45:34 +0530

    update index.html

--- script.js
+++ script.js
@@ -34,43 +34,6 @@ $(document).ready(function(){
     });
     
 
-    // typing text animation script
-    // var typed = new Typed(".typing", {
-    //     strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
-    //     typeSpeed: 100,
-    //     backSpeed: 60,
-    //     loop: true
-    // });
-
-    // var typed = new Typed(".typing-2", {
-    //     strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
-    //     typeSpeed: 100,
-    //     backSpeed: 60,
-    //     loop: true
-    // });
-
-    // // owl carousel script
-    // $('.carousel').owlCarousel({
-    //     margin: 20,
-    //     loop: true,
-    //     autoplay: true,
-    //     autoplayTimeOut: 2000,
-    //     autoplayHoverPause: true,
-    //     responsive: {
-    //         0:{
-    //             items: 1,
-    //             nav: false
-    //         },
-    //         600:{
-    //             items: 2,
-    //             nav: false
-    //         },
-    //         1000:{
-    //             items: 3,
-    //             nav: false
-    //         }
-    //     }
-    // });
 });
 document.getElementById("download-link").addEventListener("click", function () {
     alert("Your CV is downloading...");
